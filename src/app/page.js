73f89ceca144850b42b7ea4d052ec9858db65d5c
SmittyHamm <smittyhamm@Chloes-MacBook-Air.local>
2023-07-17 12:09:02 -0700

    container fluid

--- src/app/page.js
+++ src/app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
   };
 
   return (
-        <div class="container">
+        <div class="container-fluid">
           <div class="row">
               <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
                   <Image
